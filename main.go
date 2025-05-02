4f455434af6c1517d1b76e980a777b6acf2155ca
mihaibuzgau <mihai.buzgau@snyk.io>
2025-05-02 18:04:13 +0300

    remove vulns

--- main.go
+++ main.go
@@ -2,13 +2,10 @@ package main
 
 import (
 	"fmt"
-
-	"github.com/mattermost/mattermost-plugin-playbooks/server/api"
 )
 
 func main() {
 	fmt.Println("hello world")
 
-	h := api.Handler{}
-	fmt.Println(h)
+	fmt.Println("a")
 }
